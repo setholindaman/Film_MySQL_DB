2146fcea1e9f0957b195a55fc12c9b7a3db39eb4
Seth <setholindaman@gmail.com>
2019-06-25 12:39:06 -0600

    made corrections to database

--- films.sql
+++ films.sql
@@ -1,7 +1,7 @@
 use sakila;
 
 -- Display the first and last names of all actors from the table actor.
-select first_name, last_name from actor;
+select last_name, first_name from actor;
 
 -- Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 alter table actor
@@ -90,6 +90,7 @@ SELECT staff.first_name, staff.last_name, sum(payment.amount) "total amount"
 FROM payment
 INNER JOIN staff ON
 staff.staff_id=payment.staff_id
+WHERE MONTH(payment.payment_date) = 08 AND YEAR(payment.payment_date) = 2005
 group by staff.staff_id;
 
 -- List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
@@ -116,10 +117,17 @@ group by customer_id
 order by last_name asc;
 
 -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
-select title, language_id
+select title
 from film 
 where title
-like 'K%' or 'Q%';
+like 'K%' or 'Q%'
+and language_id IN
+  (
+   SELECT language_id
+   FROM language
+   WHERE name = 'English'
+  );
+;
 
 -- Use subqueries to display all actors who appear in the film Alone Trip
 select actor.first_name, actor.last_name "Actor"
@@ -154,7 +162,7 @@ select title, rental_duration from film
 order by rental_duration desc;
 
 -- Write a query to display how much business, in dollars, each store brought in.
-select store.store_id "Store", sum(payment.amount) "Amount of Business"
+select store.store_id "Store", sum(payment.amount) as "Amount of Business"
 from store
 join staff
 on store.store_id = staff.store_id
@@ -193,18 +201,18 @@ limit 5;
 -- In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. 
 -- Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
 create view top_five_genres as 
-select category.name, sum(payment.amount) as 'Gross Revenue'
-from category
-join film_category
-on category.category_id = film_category.category_id
-join inventory
-on inventory.film_id = film_category.film_id
-join rental
-on rental.inventory_id = inventory.inventory_id
-join payment
+select category.name as 'Genre', sum(payment.amount) as 'Gross Revenue'
+from payment
+inner join rental
 on payment.rental_id = rental.rental_id
+inner join inventory
+on rental.inventory_id = inventory.inventory_id
+inner join film_category
+on inventory.film_id = film_category.film_id
+inner join category
+on film_category.category_id = category.category_id
 group by category.name
-order by 'Gross Revenue' desc
+order by sum(payment.amount) desc
 limit 5;
 
 -- How would you display the view that you created in 8a?
